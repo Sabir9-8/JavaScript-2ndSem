3a79d5824d717f2cedfe8eacc52a4e7fa685dc7c
Md Sabir Ahamed <mdahamedsabir98@gmail.com>
2026-04-16 07:17:50 +0530

    dynamically show array content

--- src/apr15Array.js
+++ src/apr15Array.js
@@ -1,18 +1,16 @@
-let arr = [1, 2, 3, 4, 5];
-
-
 let users = [
     {id: 10, name: "sachin", age: 55},
     {id: 7, name: "dhoni", age: 43},
     {id: 18, name: "kohli", age: 37},
     {id: 19, name: "dravid", age: 51},
-    {id: 43, name: "rohit", age: 35}
+    {id: 43, name: "rohit", age: 35},
+    {id: 30, name: "jadeja", age: 36}
 ]
 
 let root = document.getElementById("root");
 let ul = document.createElement("ul");
 ul.style.padding = "2rem";
-
+/*
 users.filter(function (user) {
     return user.age >= 50;
 }).map(function (user) {
@@ -23,3 +21,21 @@ users.filter(function (user) {
     ul.appendChild(li);
 });
 root.appendChild(ul);
+*/
+
+let handler = function() {
+    ul.innerHTML = "";
+    users.forEach(function (user) {
+        let li = document.createElement("li");
+        li.textContent = user.name.toUpperCase();
+        ul.appendChild(li);
+    })
+    root.appendChild(ul);
+}
+handler();
+
+users = users.filter(function (user) {
+    return user.age >= 50;
+});
+
+setTimeout(handler, 5000);
