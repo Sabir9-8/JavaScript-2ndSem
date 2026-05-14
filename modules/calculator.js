ca5bddf17ec2b218ba22489518e613202d017d75
Md Sabir Ahamed <mdahamedsabir98@gmail.com>
2026-05-14 21:10:42 +0530

    intro to modules in JS

---
+++ modules/calculator.js
@@ -0,0 +1,17 @@
+export function add(a, b) {
+    return a + b;
+}
+
+function sub(a,b) {
+    return b - a;
+}
+
+export function mul(a, b) {
+    return a * b;
+}
+
+function div(a, b) {
+    return a / b;
+}
+
+export const PI = 3.14;
